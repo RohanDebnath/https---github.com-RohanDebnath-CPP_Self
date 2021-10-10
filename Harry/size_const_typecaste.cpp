c13b8c7071fe884ed6e7fb47297020c7fdf9f3f2
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2021-10-10 20:52:33 +0530

    third

---
+++ Harry/size_const_typecaste.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Size of 34.4 is"<<sizeof(34.4)<<endl;
+    cout<<"Size of 34.4f is"<<sizeof(34.4f)<<endl;
+    cout<<"Size of 34.4F is"<<sizeof(34.4F)<<endl;
+    cout<<"Size of 34.4l is"<<sizeof(34.4l)<<endl;
+    cout<<"Size of 34.4L is"<<sizeof(34.4L)<<endl;
+    cout<<"\n\n";
+    float b=90.48;
+    cout<<b<<endl;
+  //  int b;
+    cout<<b<<endl;
+    cout<<"\n\n";
+    const float a=34.453;
+    cout<<a<<endl;
+   // int a;
+    cout<<a<<endl;
+
+
+}
