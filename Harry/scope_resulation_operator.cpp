4824cc34d557726abd0440e608ed87685fdfaf38
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2021-10-10 19:32:03 +0530

    2nd

---
+++ Harry/scope_resulation_operator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int ab=10;
+int main()
+{
+    int a=10;
+    int b=90;
+    int ab=a*b; // with int , without int , onlt ab , many more; observe .
+    cout<<ab<<endl;
+    cout<<::ab;
+}
